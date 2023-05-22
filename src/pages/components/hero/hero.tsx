d31a82c9354a9edb5c873ffc0f0196337d72045d
axmadullo123 <axmadullo2000@gmail.com>
2023-05-22 15:40:38 +0500

    Hero Component has finished

--- src/pages/components/hero/hero.tsx
+++ src/pages/components/hero/hero.tsx
@@ -3,6 +3,7 @@ import {HeroProps} from "@/pages/components/hero/hero.interface";
 import {IMovies} from "@/interfaces/app.interface";
 import Image from "next/image";
 import {tmdbBaseUrl} from "@/helpers/constants";
+import ReactStars from 'react-stars'
 
 function Hero({trending}: HeroProps): JSX.Element {
     const [movie, setMovie] = useState<IMovies>({} as IMovies)
@@ -12,8 +13,17 @@ function Hero({trending}: HeroProps): JSX.Element {
         setMovie(randomMovie)
     }, [trending])
 
+    console.log(movie)
+
     return (
         <div className={'flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end'}>
+            <div className={'capitalize rounded bg-[#1D1D1D] w-[111px] text-center text-white p-[4px 8px]'}>
+                {movie?.media_type}
+            </div>
+            <div className={'flex space-x-0 items-center'}>
+                <ReactStars count={10} value={movie?.vote_average} edit={false} size={18} color1={'#b3b3b3'} color2={'white'}/>
+                <p>({movie?.vote_count})</p>
+            </div>
             <div className={'absolute -z-10 top-0 left-0 h-[95vh] w-full'}>
                  <Image
                     src={`${tmdbBaseUrl}${movie?.backdrop_path || movie?.poster_path}`}
@@ -22,10 +32,11 @@ function Hero({trending}: HeroProps): JSX.Element {
             </div>
             <div className={''}>
                 <h1 className={'text-white text-2xl font-bold md:text-4xl lg:text-7xl mb-2'}>{movie.title}</h1>
-                <p className={'max-w-xs md:max-w-lg lg:max-w-2xl text-xs md:text-lg lg:text-xl drop-shadow-lg shadow-blue-600'}>{movie?.overview}</p>
+                <p className={'max-w-xs md:max-w-lg lg:max-w-2xl text-xs md:text-lg lg:text-xl shadow_text'}>
+                    {movie?.overview?.slice(0, 100)}...</p>
             </div>
             <div>
-                <button className={'flex justify-center items-center space-x-2 border-red-500 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500  border-solid border-2 w-[200px] h-[56px] rounded-full'}>
+                <button className={'flex justify-center items-center space-x-2 border-red-500 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500  border-solid border-2 w-[150px] h-[56px] rounded-full md:w-[200px] '}>
                 <Image src={'./play.svg'} alt={'play'} width={40} height={40} className={'w-6 h-6'} /> Watch now</button>
             </div>
         </div>
